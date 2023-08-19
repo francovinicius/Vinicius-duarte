203895aaf5766aa38aa6d8dd293c2d7968b59df2
francovinicius <duartevini.dev@gmail.com>
2023-08-19 12:44:53 -0300

    alt sobre mim

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -16,7 +16,7 @@ export default function About() {
                 <p className='p-about'>Já no âmbito da tecnologia, venho aprimorando meus conhecimentos em front-end, prezando sempre em entregar meus projetos com design responsivo começando pelo mobile (Mobile First). Tenho conhecimentos em HTML5, CSS3, JavaScript, e atualmente, venho aprofundando meus estudos nos frameworks React e Bootstrap.</p>
                 <p className='p-about'>Hoje, além de trabalhar com desenvolvimento web, sou responsável por uma equipe que coordena toda a parte de banco de dados da empresa que presto serviços, juntamente com a tratativa e cruzamento de dados do sistema de ponto integrado.</p>
                 <p className='p-about'>Sou formado em Matemática e estou cursando Análise e Desenvolvimento de Sistemas. Divido meu tempo livre em momentos de lazer com a família, igreja, amo tocar violão (montar aquela roda de amigos e fazer um som), gosto de frequentar a academia pela manhã e por último mas não menos importante gosto bastante de fazer trilhas e realizar esportes radicais.</p>
-                <p className='p-about'>Estou sempre bem animado em conhecer pessoas novas, novos ensinamentos e novas culturas! Caso queira saber mais, entre em contato comigo. Um grande abraço!</p>
+                <p className='p-about'>Estou sempre bem animado em conhecer pessoas novas, novos ensinamentos e novas culturas! Caso queira saber mais, entre em contato comigo através da seção contatos. Um grande abraço!</p>
             </div>
 
         </section>
